dbf41a278550253d6247e4531bcb58f1cab91904
Yurii <yurij.kenu@gmail.com>
2026-03-20 14:24:08 +0100

    main: add script task

--- workflow/src/main/java/com/keniu/MyDelegate.java
+++ workflow/src/main/java/com/keniu/MyDelegate.java
@@ -4,11 +4,15 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class MyDelegate implements JavaDelegate {
+    private static Random random = new Random(47);
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-        System.out.println("Do work in MyDelegate");
+        delegateExecution.setVariable("myVariable", random.nextInt());
+        System.out.println("My random number is: " + delegateExecution.getVariable("myVariable"));
     }
 }
